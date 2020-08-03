a34166dda15669f00cbe2e23b5e223dc10905e86
Cesar Gomez <zootechdrum@gmail.com>
2020-08-02 21:54:16 -0700

    activiated service worker and can now cache html files for site

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,42 @@
 const FilesToCache = ["/", "./login", "./members"];
+const CACHE_NAME = "static-cache-v2";
 
 self.addEventListener("install", (event) => {
-  console.log(event);
   event.waitUntil(
-    caches.open("v1").then((cache) => {
+    caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(FilesToCache);
     })
   );
+  self.skipWaiting();
 });
 
-//This is is a comments
+// activate
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      console.log(keyList);
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+// fetch
+self.addEventListener("fetch", function (evt) {
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        console.log(response);
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
